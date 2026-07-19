8374994b007fc1254a242793b4e3e6fa51f9f5f1
RSL <tirvoxik@gmail.com>
2026-07-19 13:12:16 +0500

    Update tirvoxhub.lua

--- tirvoxhub.lua
+++ tirvoxhub.lua
@@ -37,8 +37,7 @@ Discord()
 
 -- Загрузка скриптов под конкретные игры
 if game.PlaceId == 537413528 then -- Build A Boat For Treasure 🌊
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/tirvox/babft/refs/heads/main/farm1.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/tirvox/babft/main/farm1.lua'))()
 elseif game.PlaceId == 13924946576 then -- Dingus 🔧
-    -- ВСТАВЬ СВОЮ ССЫЛКУ НА СКРИПТ СЮДА:
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/tirvox/tirvoxdingus/blob/main/tirvoxdingus.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/tirvox/tirvoxdingus/main/tirvoxdingus.lua'))()
 end
